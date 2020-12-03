9038ec89bbc8d715c01ad5b36b40006e6c9e5c39
Jacopo Carotenuto <carotenuto.jacopo00@gmail.com>
2020-12-03 11:20:02 +0100

    Updated weather data

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = '55dc851a9f4b62930f61388c06a99e51';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 45.422860;
+    let longitude = 11.898470;
 
     getWeather(latitude, longitude);
 }
